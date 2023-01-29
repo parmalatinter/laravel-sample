1722425e80ef512906d7a4cc607a61b7bc4ff9db
Yosuke Kobayashi <parmalatinter@gmail.com>
2023-01-29 09:33:22 +0900

    compiled with vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,10 +3,3 @@ require('admin-lte');
 require('jquery');
 require('datatables.net-bs5');
 require('datatables.net-buttons-bs5');
-
-import { createApp } from 'vue/dist/vue.esm-bundler.js';
-window.Vue = createApp();
-
-window.Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
-window.Vue.mount('#app');
--- resources/js/views/products.js
+++ resources/js/views/products.js
@@ -1,3 +1,10 @@
 $(document).ready(function () {
     $('#products-table').DataTable();
 });
+
+import { createApp } from 'vue/dist/vue.esm-bundler.js';
+window.Vue = createApp();
+
+window.Vue.component('example-component', require('../components/ExampleComponent.vue').default);
+
+window.Vue.mount('#app');
