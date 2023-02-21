880e0d76f8bc037d95053d5ea27c553415bf594d
Yosuke Kobayashi <parmalatinter@gmail.com>
2023-02-21 23:29:38 +0900

    add handler api

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,32 +3,28 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpFoundation\Response as HttpResponse;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
     /**
-     * A list of exception types with their corresponding custom log levels.
-     *
-     * @var array<class-string<\Throwable>, \Psr\Log\LogLevel::*>
-     */
-    protected $levels = [
-        //
-    ];
-
-    /**
      * A list of the exception types that are not reported.
      *
-     * @var array<int, class-string<\Throwable>>
+     * @var array
      */
     protected $dontReport = [
         //
     ];
 
     /**
-     * A list of the inputs that are never flashed to the session on validation exceptions.
+     * A list of the inputs that are never flashed for validation exceptions.
      *
-     * @var array<int, string>
+     * @var array
      */
     protected $dontFlash = [
         'current_password',
@@ -43,8 +39,47 @@ class Handler extends ExceptionHandler
      */
     public function register()
     {
-        $this->reportable(function (Throwable $e) {
-            //
+        $this->reportable(function (Throwable $e, Request $request) {
+            if ($request->is('api/*') || $request->ajax()) {
+                Log::error('[API Error] ' . $request->method() . ': ' . $request->fullUrl());
+            }else{
+                Log::error('[Error] ' . $request->method() . ': ' . $request->fullUrl());
+            }
+
+            switch (true) {
+                case $e instanceof ValidationException:
+                    Log::error($e->errors());
+                    break;
+                case $e instanceof HttpExceptionInterface:
+                default:
+                    $message = $e->getMessage() ? : HttpResponse::$statusTexts[$e->getStatusCode()];
+                    Log::error($message);
+                    break;
+            }
+        });
+
+        $this->renderable(function (Throwable $e, Request $request) {
+
+            if ($request->is('api/*') || $request->ajax()){
+
+                switch (true) {
+                    case $e instanceof HttpExceptionInterface:
+                        $message = $e->getMessage() ? : HttpResponse::$statusTexts[$e->getStatusCode()];
+                        Log::error($message);
+
+                        return response()->json([
+                            'message' => $message
+                        ], $e->getStatusCode());
+                    case $e instanceof ValidationException:
+                        Log::error($e->errors());
+
+                        return $this->invalidJson($request, $e);
+                    default:
+                        return response()->json([
+                            'message' => 'Internal Server Error'
+                        ], 500);
+                }
+            }
         });
     }
 }
